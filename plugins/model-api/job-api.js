1da9dbe1243d00dfe55c232ae81dc6a3527bbe32
ducanhtttvn <162439751+anhld2998@users.noreply.github.com>
2024-05-25 14:30:40 +0700

    feat: Add API

--- plugins/model-api/job-api.js
+++ plugins/model-api/job-api.js
@@ -1,11 +1,11 @@
-export default ({ appConfig, $moment, $filters, $_ }) => {
+export default (appConfig, $moment, $filters, $_ ) => {
     const getListJob = async (option = {}) => {
         //Merge options.
         const fetchOption = {
           ...option,
           method: 'GET',
         };
-        return await useFetch(`https://api.jobspeeds.com/file/lists`, fetchOption);
+        return await useFetch(`${appConfig.apiURL.API}/file-manager/list`, fetchOption);
       }
       return {
         getListJob,
