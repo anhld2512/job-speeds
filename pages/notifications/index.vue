ebc0c68f47d05e8a5a654ee841041d89b9d7e424
anhld2998 <anh.leduc2512@gmail.com>
2024-06-08 12:00:13 +0700

    fix

--- pages/notifications/index.vue
+++ pages/notifications/index.vue
@@ -2,10 +2,12 @@
   <div class="flex-row flex-wrap gap-3">
     <h1>Nuxt 3 Web Push Notifications</h1>
     <button @click="notifyUser" class="btn btn-sm btn-accent mx-1">Send Notification</button>
+    <button @click="registerForPushNotifications" class="btn btn-sm btn-accent mx-1">Register</button>
   </div>
 </template>
 
 <script setup>
+
 const { $api, $sendNotification, $urlBase64ToUint8Array } = useNuxtApp();
 
 const registerForPushNotifications = async () => {
@@ -28,12 +30,12 @@ const registerForPushNotifications = async () => {
         // Tạo đăng ký mới
         subscription = await swReg.pushManager.subscribe({
           userVisibleOnly: true,
-          applicationServerKey: urlBase64ToUint8Array('BA5xnicrkH0hO5H0Y3cK5AAik0G_j62c8mukA0eYjhQ9ShDxDvh9gksXEL-VRMoZaeT2bDh2y_1Wi2C3ro9d_9E')
+          applicationServerKey: $urlBase64ToUint8Array('BA5xnicrkH0hO5H0Y3cK5AAik0G_j62c8mukA0eYjhQ9ShDxDvh9gksXEL-VRMoZaeT2bDh2y_1Wi2C3ro9d_9E')
         });
       }
 
       // Gửi đăng ký lên server
-      await axios.post('/api/notifications/save-subscription', subscription);
+      await $api.post('/notifications/save-subscription', subscription);
       alert('Subscribed to push notifications');
     } catch (error) {
       console.error('Failed to subscribe the user: ', error);
@@ -44,17 +46,6 @@ const registerForPushNotifications = async () => {
   }
 };
 
-const urlBase64ToUint8Array = (base64String) => {
-  const padding = '='.repeat((4 - base64String.length % 4) % 4);
-  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-};
-
 function notifyUser() {
   const title = 'Hello';
   const message = 'This is a test notification';
@@ -62,10 +53,9 @@ function notifyUser() {
 }
 
 onMounted(() => {
-    nextTick().then(() => {
-        setTimeout(() => {
-          registerForPushNotifications();   
-        }, 1);
-    });
-})
+  nextTick().then(() => {
+    setTimeout(() => {
+    }, 1);
+  });
+});
 </script>
