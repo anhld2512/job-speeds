979240c5dbc5c90aed290f5d2862280f9b76afb8
anhld2998 <anh.leduc2512@gmail.com>
2024-06-08 22:50:18 +0700

    fixx

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,28 @@
 
 self.addEventListener('push', function(event) {
-    console.log('[Service Worker] Push Received.');
-    const data = event.data.json();
-  
-    const title = data.title || 'Default Title';
-    const options = {
-      body: data.message || 'Default message body',
-      icon: './favicon.ico',
-      badge: './favicon.ico',
-      data: {
-        url: data.url || 'https://default.url' // Include the URL in the notification data
-      }
-    };
-  
-    event.waitUntil(
-      self.registration.showNotification(title, options)
-    );
-  });
+  const data = event.data.json();
+
+  const title = data.title || 'Default Title';
+  const options = {
+    body: data.message || 'Default message body',
+    icon: '/logo/logo_250x250.png', // Đường dẫn tới biểu tượng của bạn
+    badge: '/logo/logo_250x250.png', // Đường dẫn tới biểu tượng nhỏ của bạn
+    data: {
+      url: data.url || '/' // Đảm bảo rằng URL được bao gồm trong dữ liệu thông báo
+    }
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
+});
   
   self.addEventListener('notificationclick', function(event) {
-    event.notification.close(); // Close the notification
+    console.log('[Service Worker] Notification click Received.');
+  
+    event.notification.close(); // Đóng thông báo
+  
     event.waitUntil(
-      clients.openWindow(event.notification.data.url) // Open the URL when the notification is clicked
+      clients.openWindow(event.notification.data.url) // Mở URL khi thông báo được nhấp
     );
   });
